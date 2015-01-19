98172049613dd3cc6c21605bd71874b797e91df2
Reilly <reilly@Moiras-MacBook-Pro.local>
2015-01-19 13:24:32 -0600

    finished layout and created signup and login backend

--- AppDevMod4ReillyNoonanGrant/GrinChat/app/src/main/java/com/awesome/reilly/grinchat/MainActivity.java
+++ AppDevMod4ReillyNoonanGrant/GrinChat/app/src/main/java/com/awesome/reilly/grinchat/MainActivity.java
@@ -2,6 +2,7 @@ package com.awesome.reilly.grinchat;
 
 import java.util.Locale;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
@@ -17,10 +18,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.TextView;
+
+import android.util.Log;
+import java.util.Locale;
+
+import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 
 public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -42,11 +52,13 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Intent intent = new Intent(this,LoginActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if(currentUser==null) {
+            navigateToLogin();
+        }
+        else{
+            Log.i(TAG,currentUser.getUsername());
+        }
 
         // Set up the action bar.
         final ActionBar actionBar = getSupportActionBar();
@@ -54,7 +66,7 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mSectionsPagerAdapter = new SectionsPagerAdapter(this,getSupportFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.pager);
@@ -83,6 +95,13 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         }
     }
 
+    private void navigateToLogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -99,7 +118,9 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_logout) {
+            ParseUser.logOut();
+            navigateToLogin();
             return true;
         }
 
@@ -121,75 +142,6 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
     public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
     }
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            Locale l = Locale.getDefault();
-            switch (position) {
-                case 0:
-                    return getString(R.string.title_section1).toUpperCase(l);
-                case 1:
-                    return getString(R.string.title_section2).toUpperCase(l);
-                case 2:
-                    return getString(R.string.title_section3).toUpperCase(l);
-            }
-            return null;
-        }
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
 
 }
--- AppDevMod4ReillyNoonanGrant/GrinChat/app/src/main/java/com/awesome/reilly/grinchat/SignUpActivity.java
+++ AppDevMod4ReillyNoonanGrant/GrinChat/app/src/main/java/com/awesome/reilly/grinchat/SignUpActivity.java
@@ -1,17 +1,89 @@
 package com.awesome.reilly.grinchat;
 
+import android.content.Intent;
+import android.app.AlertDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 
 public class SignUpActivity extends ActionBarActivity {
 
+    protected EditText mUsername;
+    protected EditText mEmail;
+    protected EditText mPassword;
+    protected Button mSignUpButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_sign_up);
+
+        mUsername= (EditText) findViewById(R.id.usernameField);
+        mEmail = (EditText) findViewById(R.id.emailField);
+        mPassword= (EditText) findViewById(R.id.passwordField);
+
+        mSignUpButton= (Button) findViewById(R.id.signUpButton) ;
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = mUsername.getText().toString();
+                String email= mEmail.getText().toString();
+                String password = mPassword.getText().toString();
+
+                username=username.trim();email=email.trim();password=password.trim();
+
+                if(username.isEmpty()||email.isEmpty()||password.isEmpty()){
+                    AlertDialog.Builder builder= new AlertDialog.Builder(SignUpActivity.this);
+                    builder.setMessage(R.string.sign_up_error_message0)
+                            .setTitle(R.string.sign_up_error_title0)
+                            .setPositiveButton(android.R.string.ok,null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+
+                }
+                else{
+                    setProgressBarIndeterminateVisibility(true);
+                    ParseUser newUser= new ParseUser();
+                    newUser.setUsername(username);
+                    newUser.setEmail(email);
+                    newUser.setPassword(password);
+                    newUser.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            setProgressBarIndeterminateVisibility(false);
+                            if(e==null){
+                                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                            }
+                            else{
+                                AlertDialog.Builder builder= new AlertDialog.Builder(SignUpActivity.this);
+                                builder.setMessage(e.getMessage())
+                                        .setTitle(R.string.sign_up_error_title0)
+                                        .setPositiveButton(android.R.string.ok,null);
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        }
+                    });
+
+                }
+
+            }
+        });
     }
 
 
